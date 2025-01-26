3286b708ed633b6c486a6dc52023755920265353
lekazz <134833137+kaizel12@users.noreply.github.com>
2025-01-26 13:15:02 +0700

    Update episode.js

--- pages/api/episode.js
+++ pages/api/episode.js
@@ -26,6 +26,9 @@ async function getEpisodeDetails(url) {
     const { data } = await axios.get(url, {
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive",
       },
     });
     const $ = cheerio.load(data);
